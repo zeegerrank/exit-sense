b6145af19333351bbde88bf02ed3584722e20f5c
zeegerrank <zeegerrank@gmail.com>
2025-06-15 20:44:01 +0700

    Refactor: simplify export of throwWith function and remove unnecessary module.exports

--- server/utils/throwWith.js
+++ server/utils/throwWith.js
@@ -1,8 +1,6 @@
-function throwWith(status, message, name = "Application error") {
+exports.throwWith = (status, message, name = "Application error") => {
   const error = new Error(message);
   error.status = status;
   error.name = name;
   throw error;
-}
-
-module.exports = { throwWith };
+};
